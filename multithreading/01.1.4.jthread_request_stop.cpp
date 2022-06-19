13f510960aae0d03165bc16c9471a97ec99858db
Volodymyr Nikolaichuk <nikolaychuk.volodymyr@gmail.com>
2022-06-19 21:39:45 +0300

    jthread request stop

---
+++ multithreading/01.1.4.jthread_request_stop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+
+void infinite_loop(std::stop_token token)
+{
+    int counter = 0;
+
+    while (!token.stop_requested())
+    {
+        ++counter;
+        std::cout << counter << std::endl;
+    }
+}
+
+int main(int argc, const char** argv)
+{
+    std::jthread t(infinite_loop);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    t.request_stop();
+
+    return 0;
+}
