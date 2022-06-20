7d7172619014d734a2dfc801a0879ddb130f1e2f
Volodymyr Nikolaichuk <nikolaychuk.volodymyr@gmail.com>
2022-06-21 01:07:43 +0300

    extend condition_variable sample

--- multithreading/04.4.1.condition_variable.cpp
+++ multithreading/04.4.1.condition_variable.cpp
@@ -8,20 +8,31 @@ bool data_ready = false;
 
 int main(int argc, const char** argv)
 {
-    std::jthread thread([]{
+    std::thread thread([]{
+        // simulate smaller work
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
         std::cout << "waiting for data" << std::endl;
 
         std::unique_lock lock(mutex);
-        condition_variable.wait(lock, []{return data_ready;});
+        condition_variable.wait(lock, []{
+            return data_ready;
+        });
         lock.unlock();
 
         std::cout << "data is ready" << std::endl;
     });
 
+    // simulate some work
     std::this_thread::sleep_for(std::chrono::seconds(1));
     
+    mutex.lock();
     data_ready = true;
+    mutex.unlock();
+
     condition_variable.notify_one();
+
+    thread.join();
 
     return 0;
 }
---
+++ multithreading/04.4.2.condition_variable.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mutex;
+std::condition_variable condition_variable;
+bool job_done = false;
+
+int main(int argc, const char** argv)
+{
+    std::thread thread([]{
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        mutex.lock();
+        job_done = true;
+        mutex.unlock();
+
+        condition_variable.notify_one();
+
+        std::cout << "job is done, cleaning up" << std::endl;
+    });
+
+    std::cout << "waiting for data" << std::endl;
+
+    // simulate smaller work
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::unique_lock lock(mutex);
+    condition_variable.wait(lock, []{
+        return job_done;
+    });
+    lock.unlock();
+
+    std::cout << "finishing" << std::endl;
+
+    thread.join();
+
+    
+
+    return 0;
+}
